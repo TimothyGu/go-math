15d211dc90dab621507a4ccf85d0cecee428ed81
Timothy Gu <timothygu99@gmail.com>
2020-07-27 22:59:55 -0400

    Add better docs

--- v2/doc.go
+++ v2/doc.go
@@ -1,2 +1,5 @@
 // Package math contains some utilities not found in the built-in math package.
+// The interesting content is in the subdirectories of this package, which are
+// organized according to the specific type in question: imath for int, u64math
+// for uint64, etc.
 package math
